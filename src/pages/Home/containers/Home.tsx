1e76dd106dae816dc78931313b5d189281610646
Tsanta Fitiavana <tsanta.ratolojanahary@versusmind.eu>
2024-05-30 17:46:18 +0300

    fix: fix height overflow

--- src/pages/Home/containers/Home.tsx
+++ src/pages/Home/containers/Home.tsx
@@ -5,7 +5,7 @@ export const Home = () => {
     return (
         <div className="h-screen flex">
             <SideMenu />
-            <div className="flex-grow bg-gray-100 p-4">
+            <div className="flex-grow bg-gray-100 p-4 max-h-[95vh]">
                 <Outlet />
             </div>
         </div>

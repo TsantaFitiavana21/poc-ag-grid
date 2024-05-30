4b9ad3f55ad11b86e5e1497d0c09202547204e17
Tsanta Fitiavana <tsanta.ratolojanahary@versusmind.eu>
2024-05-30 17:52:16 +0300

    fix: optimize routing

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,35 +1,47 @@
+import { useNavigate } from "react-router"
 import { CandidateIcon } from "../icons/CandidateIcon"
 import { JobIcon } from "../icons/JobIcon"
 import { SettingsIcon } from "../icons/SettingsIcon"
 
 export const SideMenu = () => {
+    const navigate = useNavigate()
+
     return (
         <div className="w-64 bg-gray-800 text-white p-6">
-            <ul>
-                <li className="mb-6 hover:text-blue-300">
-                    <a href="/" className="text-4xl font-bold ">
+            <div>
+                <div className="mb-6 hover:text-blue-300">
+                    <div
+                        onClick={() => navigate("/")}
+                        className="text-4xl font-bold "
+                    >
                         JobK
-                    </a>
-                </li>
-                <li className="mb-4 flex items-center space-x-2 hover:text-blue-300">
+                    </div>
+                </div>
+                <div className="mb-4 flex items-center space-x-2 hover:text-blue-300 cursor-pointer">
                     <CandidateIcon />
-                    <a href="/candidates" className="text-lg ">
+                    <div
+                        onClick={() => navigate("/candidates")}
+                        className="text-lg"
+                    >
                         Candidates
-                    </a>
-                </li>
-                <li className="mb-4 flex items-center space-x-2 hover:text-blue-300">
+                    </div>
+                </div>
+                <div className="mb-4 flex items-center space-x-2 hover:text-blue-300 cursor-pointer">
                     <JobIcon />
-                    <a href="/jobs" className="text-lg ">
+                    <div onClick={() => navigate("/jobs")} className="text-lg">
                         Jobs
-                    </a>
-                </li>
-                <li className="mb-4 flex items-center space-x-2 hover:text-blue-300">
+                    </div>
+                </div>
+                <div className="mb-4 flex items-center space-x-2 hover:text-blue-300 cursor-pointer">
                     <SettingsIcon />
-                    <a href="/settings" className="text-lg ">
+                    <div
+                        onClick={() => navigate("/settings")}
+                        className="text-lg"
+                    >
                         Settings
-                    </a>
-                </li>
-            </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

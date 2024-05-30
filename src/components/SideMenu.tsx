b95528780d75fa2995088747ba398e9fba893040
Tsanta Fitiavana <tsanta.ratolojanahary@versusmind.eu>
2024-05-30 16:59:05 +0300

    feat: adding icons to left menu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,3 +1,7 @@
+import { CandidateIcon } from "../icons/CandidateIcon"
+import { JobIcon } from "../icons/JobIcon"
+import { SettingsIcon } from "../icons/SettingsIcon"
+
 export const SideMenu = () => {
     return (
         <div className="w-64 bg-gray-800 text-white p-6">
@@ -7,14 +11,22 @@ export const SideMenu = () => {
                         JobK
                     </a>
                 </li>
-                <li className="mb-6">
+                <li className="mb-4 flex items-center space-x-2">
+                    <CandidateIcon />
                     <a href="/" className="text-lg hover:underline">
                         Candidates
                     </a>
                 </li>
-                <li className="mb-2">
+                <li className="mb-4 flex items-center space-x-2">
+                    <JobIcon />
                     <a href="/jobs" className="text-lg hover:underline">
                         Jobs
+                    </a>
+                </li>
+                <li className="mb-4 flex items-center space-x-2">
+                    <SettingsIcon />
+                    <a href="/jobs" className="text-lg hover:underline">
+                        Settings
                     </a>
                 </li>
             </ul>
